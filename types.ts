c5db393a51da31dfb2c62c34b9a715cd0718a0e8
Stanley Liu <liu.sta@northeastern.edu>
2021-01-13 22:43:14 -0500

    Types for implementation

---
+++ types.ts
@@ -0,0 +1,14 @@
+export type Variable = number;
+
+export type Symbol = string;
+export type Boolean = boolean;
+export type Empty = [];
+export type Pair = [Term, Term];
+
+export type AtomicTerm = Symbol | Variable | Boolean | Empty;
+export type Term = AtomicTerm | Pair;
+
+export type Association = [number, Term];
+export type Substitution = Association[]; // cannot have cycles
+
+export type Maybe<T> = T | false;
